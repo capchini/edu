8c4d40b29efdc04562eee1566e8a17038a385bbe
capchini <58196743+capchini@users.noreply.github.com>
2019-12-18 13:56:11 +0900

    Update select.js

--- pc/js/select.js
+++ pc/js/select.js
@@ -1,23 +1,63 @@
-$(function(){
-	var n;
+window.addEventListener("load", function(){
+	var dtA=document.querySelector(".main_campus .sel_local dt a");
+	var dd=document.querySelector(".main_campus .sel_local dd");
+	var ddA=document.querySelectorAll(".main_campus .sel_local dd a");
+	var option=document.querySelectorAll(".main_campus .sel_center select option");
+	var n=0;
 	var listName="";
-	$(".main_campus .sel_local dt a").click(function(e){
-		e.preventDefault();
-		$(this).toggleClass("active");
-		$(this).parent().next("dd").slideToggle(300);
-	});
-	$(".main_campus .sel_local dd a").click(function(e){
-		e.preventDefault();
-		$(".main_campus .sel_local dd a").removeClass("active");
-		$(this).addClass("active");
 
-		listName=$(this).text();
-		$(".main_campus .sel_local dt a").html(listName+"<span></span>");
-		$(".main_campus .sel_local dt a").removeClass("active");
-		$(".main_campus .sel_local dd").slideUp(300);
+	dtA.flag=false;
 
-		n=$(this).parent().index();
-		$(".main_campus .sel_center select option").prop("selected", false);
-		$(".main_campus .sel_center select option").eq(n+1).prop("selected", true);
+	dtA.addEventListener("click", function(e){
+		e.preventDefault();
+
+		if(dtA.flag == false){
+			dtA.flag=true;
+			dtA.classList.add("active");
+			dtA.parentElement.nextElementSibling.style.display="block";
+		}
+		else{
+			dtA.flag=false;
+			dtA.classList.remove("active");
+			dtA.parentElement.nextElementSibling.style.display="none";
+		}
 	});
+
+	var listN;
+
+	for(var i=0; i<ddA.length; i++){
+		ddA[i].index=i;
+
+		ddA[i].addEventListener("click", function(e){
+			e.preventDefault();
+			listN=e.currentTarget.index;
+			// console.log("listN : "+listN);
+			listName=e.currentTarget.innerHTML;
+			listName=listName+"<span></span>"
+			// console.log("listName : "+listName);
+			dtA.innerHTML=listName;
+
+			for(var j=0; j<ddA.length; j++){
+				if(j == listN){
+					ddA[j].classList.add("active");
+				}
+				else{
+					ddA[j].classList.remove("active");
+				}
+			}
+			dtA.flag=false;
+			dtA.classList.remove("active");
+			dtA.parentElement.nextElementSibling.style.display="none";
+
+			for(j=0; j<option.length; j++){
+				// console.log(j);
+				if(j == (listN+1)){
+					option[j].setAttribute("selected", true);
+				}
+				else{
+					option[j].removeAttribute("selected");
+				}
+			}
+		});
+	}
 });
