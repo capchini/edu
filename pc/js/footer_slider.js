0d2d56693c1b272e8275871192bfad4622f7c30f
capchini <58196743+capchini@users.noreply.github.com>
2019-12-18 17:16:54 +0900

    Update footer_slider.js

--- pc/js/footer_slider.js
+++ pc/js/footer_slider.js
@@ -1,21 +1,55 @@
-$(function(){
+window.addEventListener("load", function(){
 	var w=150;
 	var amount=0;
-	$(".rel_site_inner .direction a.prev").click(function(e){
+	var prev=document.querySelector(".rel_site_inner .direction a.prev");
+	var next=document.querySelector(".rel_site_inner .direction a.next");
+	var siteUl=document.querySelector(".rel_site_inner .site_wrapper ul");
+
+	prev.addEventListener("click", function(e){
 		e.preventDefault();
 		amount-=w;
-		$(".rel_site_inner .site_wrapper ul").animate({left:amount}, 500, function(){
-			$(this).append($(".rel_site_inner .site_wrapper ul li:first-child"));
-			amount+=w;
-			$(this).css({left:amount});
-		});
-	});
-	$(".rel_site_inner .direction a.next").click(function(e){
+		var current=window.getComputedStyle(siteUl).left;
+		current=parseInt(current);
+
+		var timer=setInterval(function(){
+			if(Math.abs(amount-current) > 10){
+				current-=10;
+				siteUl.style.left=current+"px";
+			}
+			else{
+				current=amount;
+				clearInterval(timer);
+				var firstchild=siteUl.firstElementChild;
+				var firstclone=firstchild.cloneNode(true);
+				firstchild.remove(true);
+				siteUl.appendChild(firstclone);
+				amount+=w;
+				siteUl.style.left=amount+"px";
+			}
+		}, 10);
+	})
+
+	next.addEventListener("click", function(e){
 		e.preventDefault();
-		$(".rel_site_inner .site_wrapper ul").prepend($(".rel_site_inner .site_wrapper ul li:last-child"));
+		var lastchild=siteUl.lastElementChild;
+		var firstchild=siteUl.firstElementChild;
+		var lastclone=lastchild.cloneNode(true);
+		lastchild.remove(true);
+		siteUl.insertBefore(lastclone, firstchild);
 		amount-=w;
-		$(".rel_site_inner .site_wrapper ul").css({left:amount});
+		siteUl.style.left=amount+"px";
 		amount+=w;
-		$(".rel_site_inner .site_wrapper ul").animate({left:amount}, 500);
+		var current=window.getComputedStyle(siteUl).left;
+		current=parseInt(current);
+		var timer=setInterval(function(){
+			if(Math.abs(current-amount) > 10){
+				current+=10;
+			}
+			else{
+				current=amount;
+				clearInterval(timer);
+			}
+			siteUl.style.left=current+"px";
+		}, 10);
 	});
 });
